ea6e3f07590d1acf25c3e43d8a8b018db3674689
Jeffin T.J <jeff.jo95z@gmail.com>
2025-08-01 16:54:35 +0530

    Update create-artifacts.sh
    added creation of channel block to join channel via API

--- HLF-Complete/FabricNetwork/artifacts/channel/create-artifacts.sh
+++ HLF-Complete/FabricNetwork/artifacts/channel/create-artifacts.sh
@@ -5,26 +5,19 @@ createArtifacts() {
 ARTIFACTS_DIR="../../channel-artifacts"
 CONFIG_PATH="."
 
-# System channel
-SYS_CHANNEL="sys-channel"
-
 # channel name defaults to "mychannel"
 CHANNEL_NAME="mychannel"
 
 # Use a loop for organizations passed as arguments
 for ORG in "$@"; do
     echo "#######    Generating anchor peer update for $ORG  ##########"
-    configtxgen -profile BasicChannel -configPath "$CONFIG_PATH" -outputAnchorPeersUpdate ./"$ORG"anchors.tx -channelID "$CHANNEL_NAME" -asOrg "$ORG"
+    configtxgen -profile ChannelUsingRaft -configPath "$CONFIG_PATH" -outputAnchorPeersUpdate ./"$ORG"anchors.tx -channelID "$CHANNEL_NAME" -asOrg "$ORG"
+
 done
 
-# Combine rm commands
-rm genesis.block mychannel.tx "$ARTIFACTS_DIR"/*
-
-# Generate System Genesis block
-configtxgen -profile OrdererGenesis -configPath "$CONFIG_PATH" -channelID "$SYS_CHANNEL" -outputBlock ./genesis.block
-
 # Generate channel configuration block
-configtxgen -profile BasicChannel -configPath "$CONFIG_PATH" -outputCreateChannelTx ./mychannel.tx -channelID "$CHANNEL_NAME"
+
+configtxgen -profile ChannelUsingRaft -configPath "$CONFIG_PATH" -channelID mychannel -outputBlock ./mychannel.block
 
 echo "Script executed successfully."
 }
